9a5ceb5482edfdbbb67014effe5dc28c6c7ed184
Souvik Naskar <46264357+Souvikavi95@users.noreply.github.com>
2018-12-30 21:11:42 +0530

    Create Trapizoidal.py

---
+++ Trapizoidal.py
@@ -0,0 +1,16 @@
+# BY USING LAMBDA 
+INPUT AS "trapizoidal (lambda x : function)"
+
+def trapizoidal(func):    
+    a = float(input("Enter Lower Limit : "))
+    b = float(input("Enter Upper Limit : "))
+    n = int(input("Enter the value of 'n': "))
+    
+    h = (b-a)/n
+    init = func(a)
+    final = func(b)
+    middle_values =0
+    for i in range (1,n):              #as range will go from 1 to n-1
+        middle_values += func(a+(i*h))
+    print (f"({h/2})*({init} + {final} + 2*{middle_values})")
+    return (h/2)*(init + final + 2*(middle_values))
